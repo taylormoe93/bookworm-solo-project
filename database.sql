881e08b9b0fea21c09597bb7c262169705e742c7
Taylor Moe <taylormoe93@gmail.com>
2022-09-06 15:31:34 -0500

    Installed npm and setup databse.

--- database.sql
+++ database.sql
@@ -8,3 +8,33 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "genres" (
+	"id" SERIAL PRIMARY KEY,
+	"name" varchar(80) NOT NULL
+); 
+
+CREATE TABLE "books" (
+	"id" SERIAL PRIMARY KEY,
+	"title" varchar(120) NOT NULL,
+	"author" varchar(120) NOT NULL,
+	"cover" varchar(350) NOT NULL,
+	"notes" varchar(350) NOT NULL,
+	"book_read" BOOLEAN NOT NULL,
+	"user_id" INT REFERENCES "user",
+	"genres_id" INT REFERENCES "genres"
+); 
+
+---- [DATA] ----
+-- Genres values
+INSERT INTO "genres" ("name")
+VALUES
+('Classics'),
+('Literary'),
+('Sci-fi'),
+('Fantasy'),
+('Mystery'),
+('Romance'),
+('Horror'),
+('Non-fiction'),
+('Poetry');
