bbaeda1bc03a35dd7c5cd18d657e54fa40d10228
Taylor Moe <taylormoe93@gmail.com>
2022-09-07 10:00:00 -0500

    Created Add Book in Nav.jsx. Still need to create route in app.jsx

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,13 +4,15 @@ import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 import { useSelector } from 'react-redux';
 
+import { Navlink } from 'react-router-dom'; // Navlink import
+
 function Nav() {
   const user = useSelector((store) => store.user);
 
   return (
     <div className="nav">
       <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
+        <h2 className="nav-title">Bookworm</h2>
       </Link>
       <div>
         {/* If no user is logged in, show these links */}
@@ -24,8 +26,13 @@ function Nav() {
         {/* If a user is logged in, show these links */}
         {user.id && (
           <>
+            {/* Formerly called HOME */}
             <Link className="navLink" to="/user">
-              Home
+              My Library
+            </Link>
+
+            <Link className="navLink" to="/add">
+              Add Book
             </Link>
 
             <Link className="navLink" to="/info">
